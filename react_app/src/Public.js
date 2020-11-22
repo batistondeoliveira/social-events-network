5628c67fcb427812107610078b88c3a7de47ac90
Eliel <eliel@avance.inf.br>
2020-11-22 20:36:33 -0300

    Changed the component by adding the BrowserRoute and the Route to register the user

--- react_app/src/Public.js
+++ react_app/src/Public.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Register from './component/user/Register';
 
-class App extends React.Component {
+import './css/Style.css'
+class Public extends React.Component {
     render() {
         return (
-            <div>
-                teste
-            </div>
+            <BrowserRouter ref={(ref) => this.browserRoute =  ref}>
+                <Switch>                                                             
+                    <Route 
+                        exact 
+                        path='/register'
+                        render={props => 
+                            <Register
+                                {...props}
+                                    
+                                route={(item) => this.browserRoute.history.push(item.link)}
+                            />
+                        }
+                    />                                                         
+                </Switch>
+            </BrowserRouter>   
         )
     }
 }
 
-export default App;
+export default Public;
