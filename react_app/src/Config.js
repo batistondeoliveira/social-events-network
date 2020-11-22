bd05341cb3e3c1ecf8cab7ccf513c533ed5e40b8
Eliel <eliel@avance.inf.br>
2020-11-22 20:32:10 -0300

    Created the Config component with the configuration of the api url and the url of the image folder

---
+++ react_app/src/Config.js
@@ -0,0 +1,6 @@
+const config = {};
+
+config.urlApi = 'https://localhost/social-events-network/api/';
+config.urlImg = 'https://localhost/social-events-network/react_app/public/img/';
+
+export default config;
