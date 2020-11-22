8d8908db8ed56210fa3f2e219014ac6372ab9783
Eliel <eliel@avance.inf.br>
2020-11-22 08:40:44 -0300

    Created the api skeleton with slim

---
+++ api/index.php
@@ -0,0 +1,60 @@
+<?php
+
+ini_set('display_errors',0); 
+ini_set('display_startup_erros',0); 
+error_reporting(E_ALL);
+
+define('_LOCAL_' , true); //mudar para false em producao
+define('_BASE_' , (_LOCAL_ == true) ?  '../' : '../app/');
+
+require _BASE_ . 'vendor/autoload.php';
+require _BASE_. 'config.php';
+
+global $config;
+
+require 'doctrine.php';
+
+global $configDoctrine;
+
+$app = new \Slim\App($config);
+
+$container = $app->getContainer();
+
+$container['dirUpload'] = (_LOCAL_) ?__DIR__ . '/../img/'  : __DIR__. '/../public_html/img/';
+
+$container['config'] = function ($container) use ($config) {
+   return $config;
+};
+
+$container['key'] = function ($container) {
+   return '87fdjfduity*';
+};
+
+$container['em'] = function ($container) use ($config, $configDoctrine) {
+   return Doctrine\ORM\EntityManager::create($config['db'], $configDoctrine);
+};
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+   return $response;
+});
+
+require _BASE_ . 'route.php';
+
+$app->add(new Slim\Middleware\JwtAuthentication([
+    "regexp" => "/(.*)/",
+    "header" => "X-Token",
+    "path" => "/",
+    "passthrough" => [          
+        
+    ],
+    "realm" => "Protected",
+    "secure" => false,
+    "secret" => $container['key'],
+    "callback" => function (\Slim\Http\Request $request, \Slim\Http\Response $response, $arguments) use ($container) {        
+        
+    }
+]));
+
+unset($config);
+
+$app->run();
---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+$config = [
+	'local' => _LOCAL_,
+	'db' => [
+		'driver' => 'pdo_mysql',
+		'host' => 'localhost:3306',
+		'dbname' => 'social_event_network',
+		'user' => 'coderock',
+		'password' => 'coderock',
+		'charset'  => 'utf8',
+        'driverOptions' => array(
+            1002 => 'SET NAMES utf8'
+        )
+	],
+	'settings' => [
+        'displayErrorDetails' => true
+  ],
+];
+
+if(!_LOCAL_) {
+    $config['db'] = [
+        'driver' => 'pdo_mysql',
+        'host' => '',
+        'dbname' => '',
+        'user' => '',
+        'password' => '',
+        'charset'  => 'utf8',
+        'driverOptions' => array(
+            1002 => 'SET NAMES utf8'
+        )
+    ];
+}
