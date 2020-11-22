e6981915dd1298bdf529ad39b8fe86107b5b8562
Eliel <eliel@avance.inf.br>
2020-11-22 09:06:57 -0300

    Changed the AbstractEntity class by adding serialize and deserialize methods to convert object to json and json to object

--- Entity/AbstractEntity.php
+++ Entity/AbstractEntity.php
@@ -4,6 +4,7 @@ namespace Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
+use JMS\Serializer\SerializerBuilder;
 use JMS\Serializer\Annotation as Serializer;
 
 use Symfony\Component\Validator\Constraints as Assert;
@@ -30,5 +31,28 @@ abstract class AbstractEntity {
     public function getId()
     {
         return $this->id;
-    }       
+    }    
+    
+    public function serialize() {
+        $serializer = SerializerBuilder::create()->build(); 
+        
+        return $serializer->toArray($this); 
+    }
+
+    public function deserialize($jsonArray) { 
+        $objAux = $this;
+
+        $serializer = SerializerBuilder::create()->build();                     
+
+        $obj = $serializer->deserialize(
+            json_encode($jsonArray, 0), 
+            $this->getClassName(), 
+            'json'
+        );                
+
+        $obj->setErrorMessage($objAux->getErrorMessage());
+        $obj->setClass($objAux->getClass());
+
+        return $obj;
+    }
 }
