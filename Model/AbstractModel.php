28223b003edba73bdd8ccc4a84f451cd74d08be0
Eliel <eliel@avance.inf.br>
2020-11-22 08:59:31 -0300

    Created AbstractModel class

---
+++ Model/AbstractModel.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Model;
+
+use Doctrine\ORM\EntityManager;
+
+abstract class AbstractModel {
+    /**
+     * @var $em EntityManager
+     */
+    private $em;   
+    
+    private $class;    
+
+	public function __construct($em, $class) {
+        $this->em = $em;        
+        $this->class = $class;        
+    }    
+    
+    /**
+     * Get entityManager
+     *
+     * @return  $em
+     */ 
+    public function getEm()
+    {
+        return $this->em;
+    }        
+
+    public function getRepository() {
+        return $this->getEm()
+            ->getRepository($this->class);
+    }    
+
+    public function save($entity) {        
+        $this->getEm()->persist($entity);
+        $this->getEm()->flush();
+    }        
+
+    public function remove($entity) {
+        $this->getEm()->remove($entity);        
+        $this->getEm()->flush();
+    }    
+    
+    public function getAll() {
+        return $this->getRepository()            
+            ->findAll();
+    }    
+
+    public function getById($id) {
+        return $this->getRepository()            
+            ->findOneBy([                
+                'id' => $id
+            ]);
+    }
+}
