e8349f97ad0afdac3eee6db356414eaf8167718e
Eliel <eliel@avance.inf.br>
2020-11-22 23:45:45 -0300

    Created Login Component

---
+++ react_app/src/component/login/Login.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import LoginContent from './LoginContent';
+
+class Login extends React.Component {                          
+    onClick() {
+        window.location.href = '/register';
+    }
+
+    render() {
+        return(
+            <div id="layoutAuthentication">                  
+                <div id="layoutAuthentication_content">
+                    <main>
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-lg-5">
+                                    <div className="card shadow-lg border-0 rounded-lg mt-5">
+                                        <div className="card-header">
+                                            <h3 className="text-center font-weight-light my-4">
+                                                Login
+                                            </h3>
+                                        </div>
+
+                                        <div className="card-body">
+                                            <LoginContent />
+                                        </div>
+
+                                        <div className="card-footer text-center">
+                                            <div className="small">
+                                                <a onClick={() => this.onClick()}>
+                                                    Cadastre-se? Inscreva-se!
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </main>
+                </div>                
+            </div>
+        )
+    }
+}
+
+export default Login;
