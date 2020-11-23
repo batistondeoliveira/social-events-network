8fc174e626610cb5ad17d8d488b37459f1b013f6
Eliel <eliel@avance.inf.br>
2020-11-22 23:47:44 -0300

    Login service created

--- react_app/src/service/UserService.js
+++ react_app/src/service/UserService.js
@@ -4,7 +4,14 @@ import RequestService from './RequestService';
 class UserService {               
     static async save(data) {                
         return RequestService.post(Config.urlApi + 'user', data);
-    }      
+    }          
+    
+    static async login(email, password) {        
+        return RequestService.post(Config.urlApi + 'user/login', {                      
+            email: email,
+            password: password
+        });
+    }
 }
 
 export default UserService;
