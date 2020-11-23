21104522bfc2374370d28ced669bddf2dc763b1b
Eliel <eliel@avance.inf.br>
2020-11-22 21:36:15 -0300

    Documentation generated from the UserController save method

---
+++ Documentation/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Social Events Network",
+  "version": "1.0.0",
+  "description": "Social Events Network for the coderock selection process",
+  "title": "Social Events Network API",
+  "url": "https://localhost/social-events-network",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-11-23T00:30:13.324Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});
