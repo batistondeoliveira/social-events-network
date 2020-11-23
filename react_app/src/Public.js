cddac11f29e3ed5ef3b75f6681caacf476a277a1
Eliel <eliel@avance.inf.br>
2020-11-22 23:46:56 -0300

    Added the route for login

--- react_app/src/Public.js
+++ react_app/src/Public.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NotFound from './component/layout/notfound/NotFound';
 import Register from './component/user/Register';
+import Login from './component/login/Login';
 
 import './css/Style.css'
 class Public extends React.Component {
@@ -19,7 +20,19 @@ class Public extends React.Component {
                                 route={(item) => this.browserRoute.history.push(item.link)}
                             />
                         }
-                    />    
+                    />  
+
+                    <Route 
+                        exact 
+                        path='/login'
+                        render={props => 
+                            <Login
+                                {...props}
+                                    
+                                route={(item) => this.browserRoute.history.push(item.link)}
+                            />
+                        }
+                    />   
 
                     <Route path="*" component={NotFound}/>                                                     
                 </Switch>
