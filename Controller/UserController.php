a23826f62e7e76e901fb8799768e6159a9ef493e
Eliel <eliel@avance.inf.br>
2020-11-22 23:08:56 -0300

    Created login method

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -15,7 +15,7 @@ class UserController extends AbstractController {
     }      
 
     /**
-     * @api {post} /user/save Save a new user
+     * @api {post} /user Save a new user
      * @apiVersion 1.0.0
      * @apiName save
      * @apiGroup user
@@ -33,9 +33,9 @@ class UserController extends AbstractController {
      *      "state": "MG"
      *    }
      * 
-     * @apiError (401) Exception Validation error message     
+     * @apiError (401) MessageError Validation error message     
      *
-     * @apiSuccess (200) {jsonArray} json Persisted user json object
+     * @apiSuccess (200) {String} token Generated validation token
      * 
      * @apiSuccessExample {json} Exemple Value
      *    {
@@ -56,7 +56,7 @@ class UserController extends AbstractController {
             $userEntity = new UserEntity();
 
             $userEntity = $userEntity->deserialize($jsonArray);
-
+            
             $userModel = new UserModel($this->container->em);
             
             $userModel->save($userEntity);                                                     
@@ -69,5 +69,43 @@ class UserController extends AbstractController {
                     $ex
                 ), 401, JSON_UNESCAPED_UNICODE);
         }
+    }
+
+    /**
+     * @api {post} /user/login User login
+     * @apiVersion 1.0.0
+     * @apiName login
+     * @apiGroup user
+     *
+     * @apiParam (parameters) {String} email User's email
+     * @apiParam (parameters) {String} password Password to login 
+     *     
+     * @apiParamExample {json} Exemple Value
+     *    {     
+     *      "email": "fulano@gmail.com",
+     *      "password": "123456",     
+     *    }
+     * 
+     * @apiError (401) MessageError Usuário e/ou senha inválido    
+     *
+     * @apiSuccess (200) {jsonArray} json Persisted user json object
+     * 
+     * @apiSuccessExample {json} Exemple Value
+     *    eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzZXNzaW9uIjoiJDJ5JDEwJHlGc0JJN2VvNjdSTmc4SU9DeHhUQk95YVBzWmpCZGZtUGxDVWtkTGdIQXFta01FNm4zT3dlIiwiaWQiOjEsImlhdCI6MTYwNjA5NjU2MywiZXhwIjoxNjA2MTAwMTYzfQ.6huB7zuB6Mo9jUWVvILad9Jd-SvTP9EqKZgmQjP6Hj4
+     */
+    public function login(Request $request, Response $response) {
+        $key = $this->container['key'];
+                
+        $email = $request->getParsedBodyParam('email');
+        $senha = $request->getParsedBodyParam('password');           
+
+        $userModel = new UserModel($this->container->em);
+        
+        $token = $userModel->login($key, $email, $senha);
+
+        if(empty($token))
+            return $response->withJson('Usuário e/ou senha inválido', 401, JSON_UNESCAPED_UNICODE);                
+
+        return $response->withJson($token, 200);        
     }
 }
