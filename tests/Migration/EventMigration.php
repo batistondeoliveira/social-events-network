d59d7615d9c9aad473b1541e19b2a2550ba22c48
Eliel <eliel@avance.inf.br>
2020-11-23 13:15:38 -0300

    Created EventMigration class

---
+++ tests/Migration/EventMigration.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace tests\Migration;
+
+use tests\Migration\AbstractMigration;
+
+class EventMigration extends AbstractMigration {
+    public function up() {
+        $this->addSql("DROP TABLE IF EXISTS event");
+
+        $this->addSql("
+            CREATE TABLE `event` (
+                `id` int(11) NOT NULL AUTO_INCREMENT,
+                `id_user` int(11) NOT NULL,
+                `name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+                `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+                `date` date DEFAULT NULL,
+                `time` time DEFAULT NULL,
+                `place` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+                PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB;
+        ");        
+    }
+}
