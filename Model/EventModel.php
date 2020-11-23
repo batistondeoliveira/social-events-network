1b4b7e9f9d64b752f9e643316fdf4468410d102e
Eliel <eliel@avance.inf.br>
2020-11-23 20:11:07 -0300

    Created getAllActiveEvent method

--- Model/EventModel.php
+++ Model/EventModel.php
@@ -10,4 +10,15 @@ class EventModel extends AbstractModel {
     public function __construct($em) {
         parent::__construct($em, EventEntity::class);
     }
+
+    public function getAllActiveEvent() {
+        $date = (new \DateTime())->format('Y-m-d');
+
+        return $this->getRepository()            
+            ->createQueryBuilder("e")
+            ->andWhere('e.date < :date')
+            ->setParameter('date', $date)            
+            ->getQuery()
+            ->getResult(); 
+    }
 }
