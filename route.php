62f78c6629421dfe25f5b4f40793ab705e06fc1d
Eliel <eliel@avance.inf.br>
2020-11-24 13:41:16 -0300

    Added the route to delete an event

--- route.php
+++ route.php
@@ -7,6 +7,6 @@ $app->post('/user/login', 'Controller\UserController:login');
 //Event
 $app->post('/event', 'Controller\EventController:save');
 $app->get('/event', 'Controller\EventController:getEventsByEmailUser');
-
+$app->delete('/event/{id}', 'Controller\EventController:delete');
 $app->get('/event/detail/{id}', 'Controller\EventController:getById');
 $app->get('/event/list', 'Controller\EventController:getAllActiveEvent');
