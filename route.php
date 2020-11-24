f4f6f188f37ab7cc0fe0f3809d44fb827266371a
Eliel <eliel@avance.inf.br>
2020-11-23 23:56:57 -0300

    Created the route for the getEventsByEmailUser method

--- route.php
+++ route.php
@@ -6,5 +6,7 @@ $app->post('/user/login', 'Controller\UserController:login');
 
 //Event
 $app->post('/event', 'Controller\EventController:save');
+$app->get('/event', 'Controller\EventController:getEventsByEmailUser');
+
 $app->get('/event/detail/{id}', 'Controller\EventController:getById');
 $app->get('/event/list', 'Controller\EventController:getAllActiveEvent');
