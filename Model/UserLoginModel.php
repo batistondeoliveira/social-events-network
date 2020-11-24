8054e28be4bd7c586bd1a8ef4072eebcfd9ddbc0
Eliel <eliel@avance.inf.br>
2020-11-23 23:55:14 -0300

    Created the getByCredentials method

--- Model/UserLoginModel.php
+++ Model/UserLoginModel.php
@@ -10,4 +10,19 @@ class UserLoginModel extends AbstractModel {
     public function __construct($em) {
         parent::__construct($em, UserLoginEntity::class);
     }    
+
+    public function getByCredentials($idUser, $token) { 
+        $date = (new \DateTime())->format('Y-m-d H:i:s');
+
+        return $this->getRepository()                        
+            ->createQueryBuilder("u")            
+            ->andWhere('u.idUser = :idUser')
+            ->setParameter('idUser', $idUser)
+            ->andWhere('u.token = :token')
+            ->setParameter('token', $token)
+            ->andWhere('u.expirar >= :date')
+            ->setParameter('date', $date)            
+            ->getQuery()
+            ->getResult();       
+    }
 }
