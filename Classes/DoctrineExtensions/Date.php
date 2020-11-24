ad837e688de29db86db553c6ee6da75bd015ee78
Eliel <eliel@avance.inf.br>
2020-11-23 21:25:22 -0300

    Created Date class to use mysql date formatting in doctrine

---
+++ Classes/DoctrineExtensions/Date.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Classes\DoctrineExtensions;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+class Date extends FunctionNode
+{
+    public $dateExpression = null;
+
+     public function parse(Parser $parser)
+     {
+         $parser->match(Lexer::T_IDENTIFIER);
+         $parser->match(Lexer::T_OPEN_PARENTHESIS);
+         $this->dateExpression = $parser->ArithmeticPrimary();
+         $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+     }
+
+     public function getSql(SqlWalker $sqlWalker)
+     {
+         return \sprintf('DATE(%s)', $this->dateExpression->dispatch($sqlWalker));
+     }
+}
