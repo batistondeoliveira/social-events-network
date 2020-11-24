e1950f4116d2fbe9effb04847614576612c46a33
Eliel <eliel@avance.inf.br>
2020-11-23 23:58:10 -0300

    Changed the route / admin to display the list of logged user events

--- react_app/src/Admin.js
+++ react_app/src/Admin.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MyEvents from './component/event/MyEvents';
+import NotFound from './component/layout/notfound/NotFound';
 
 import './css/Style.css'
 
-class Admin extends React.Component {    
+class Admin extends React.Component {        
     render() {
         return (
-            <div>
-                aaa
-            </div>  
+            <BrowserRouter ref={(ref) => this.browserRoute =  ref}>
+                <Switch>   
+                    <Route 
+                        exact 
+                        path='/admin'
+                        render={props => 
+                            <MyEvents
+                                {...props}
+                                    
+                                route={(item) => this.browserRoute.history.push(item.link)}
+                            />
+                        }
+                    />                    
+
+                    <Route path="*" component={NotFound}/>                                                     
+                </Switch>
+            </BrowserRouter>  
         )
     }
 }
