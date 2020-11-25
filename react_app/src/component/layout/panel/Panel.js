69219e5046ba542a77c981da2590b7e98f8fcb12
Eliel <eliel@avance.inf.br>
2020-11-25 15:01:34 -0300

    Created Panel component

---
+++ react_app/src/component/layout/panel/Panel.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Col} from 'react-bootstrap';
+   
+class Panel extends React.Component {        
+    render() {
+        return (
+            <Col 
+                md={this.props.md} 
+                xs={this.props.xs} 
+                sm={this.props.sm} 
+                lg={this.props.lg}
+                style={{
+                    paddingLeft: '0px',
+                    paddingRight: '0px'
+                }}
+            >
+                <div className="card mt-3 mb-3">
+                    <div className="card-header">                        
+                        {this.props.title}
+                    </div>
+
+                    <div className="card-body">
+                        {this.props.children}
+                    </div>
+                </div>                
+            </Col>
+        )
+    }
+}
+
+export default Panel;
