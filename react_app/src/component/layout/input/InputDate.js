8dfc651cee30a3851cda94069540548f922d0ad9
Eliel <eliel@avance.inf.br>
2020-11-25 10:05:57 -0300

    Created InputDate and InputTime components

---
+++ react_app/src/component/layout/input/InputDate.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Input from './Input';
+
+class InputDate extends React.Component {
+    onChange(input) {                   
+        var inputStr = input.target.value;
+        
+        inputStr = inputStr.replace(/\D/g,"");        
+        inputStr = inputStr.replace(/(\d{2})(\d)/,"$1/$2");
+        inputStr = inputStr.replace(/(\d{2})(\d)/,"$1/$2");
+        inputStr = inputStr.replace(/(\d{2})(\d{2})$/,"$1$2");    
+
+        input.target.value = inputStr;
+
+        if (this.props.onChange !== undefined) {
+          this.props.onChange(input);
+        }        
+    }        
+    
+    render() {
+        return (
+            <Input
+                md={this.props.md}
+                xs={this.props.xs}
+                sm={this.props.sm}
+                lg={this.props.lg}
+                type="tel"
+                name={this.props.name}   
+                maxLength="10"                                                                  
+                classNameInput="background-transparent"  
+                value={this.props.value}
+                error={this.props.error}
+                onChange={(e) => this.onChange(e)}
+            >
+                {this.props.children}
+            </Input>
+        )
+    }
+}
+
+InputDate.defaultProps = {
+    md: 12,    
+};
+
+export default InputDate;
