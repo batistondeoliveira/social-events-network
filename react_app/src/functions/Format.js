30f29c34a0ac6993020fd70b8ee2dfd2df0a770d
Eliel <eliel@avance.inf.br>
2020-11-25 10:06:52 -0300

    Created dateFormat function

---
+++ react_app/src/functions/Format.js
@@ -0,0 +1,12 @@
+export function dateFormat(data) {
+    if (data === '' || data === null)
+        return '';
+
+    data = "" + data + "";
+
+    if (data.indexOf(':') > 1) {
+        return data.replace(/^([0-9]{4})-([0-9]{2})-([0-9]{2})(.*)$/, '$3/$2/$1 $4').substr(0, 10);
+    } else {
+        return data.replace(/^([0-9]{4})-([0-9]{2})-([0-9]{2})$/, '$3/$2/$1');
+    }
+}
