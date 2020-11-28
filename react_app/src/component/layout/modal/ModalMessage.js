7422ba2dd9423492f3b9210b3cea640f53be69b1
Eliel <eliel@avance.inf.br>
2020-11-28 00:54:18 -0300

    Correcting imports

--- react_app/src/component/layout/modal/ModalMessage.js
+++ react_app/src/component/layout/modal/ModalMessage.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {Modal} from 'react-bootstrap';
-import {Button} from 'react-bootstrap';
+import {Modal, Button} from 'react-bootstrap';
 import AbstractComponent from '../../AbstractComponent';
 import MyModal from './MyModal';
 
